0365357e427ddd2f04f7abd71f4a5da86e9edb0d
SendyMederos <sendymederos@hotmail.com>
2020-12-15 14:43:33 -0500

    Create Book Model

---
+++ models/Book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: {type: String},
+    authors: {type: Array},
+    description: {type: String},
+    image: {type: String},
+    link: {type: String}
+});
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
